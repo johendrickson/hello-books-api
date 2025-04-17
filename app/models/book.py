66d97476c5b0b03feccc33463bda0634fcb14e0b
Jamie Hendrickson <jamie.o.hendrickson@gmail.com>
2025-04-16 18:06:04 -0700

    Updated books with fun ones and their descriptions.

--- app/models/book.py
+++ app/models/book.py
@@ -5,7 +5,7 @@ class Book:
         self.description = description
 
 books = [
-    Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
-    Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
-    Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
+    Book(1, "Circe", "Circe is a 2018 mythic fantasy novel by American writer Madeline Miller. Set during the Greek Heroic Age, it is an adaptation of various Greek myths, most notably the Odyssey, as told from the perspective of the witch Circe."),
+    Book(2, "Pride and Prejudice", "A classic novel by Jane Austen that tells the story of Elizabeth Bennet and her relationship with Mr. Darcy. It's a tale of love, social expectations, and personal growth."),
+    Book(3, "The Name of the Wind", "The Name of the Wind, also referred to as The Kingkiller Chronicle: Day One, is a heroic fantasy novel written by American author Patrick Rothfuss.")
 ]
