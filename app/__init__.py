2b5a4b0a484cfcaa1ac26bd9d15d0c1ea565a1bb
Jamie Hendrickson <jamie.o.hendrickson@gmail.com>
2025-05-07 21:24:31 -0700

    Implements wave 10 of Learn lesson. Creates and sets up the Genre and BookGenre models. Creates and refactors RESTful endpoints.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from .db import db, migrate
-from .models import book, author
+from .models import book, author, genre, book_genre
 from .routes.book_routes import bp as books_bp
 from .routes.author_routes import bp as authors_bp
+
 import os
+
 
 def create_app(config=None):
     app = Flask(__name__)
@@ -22,5 +24,6 @@ def create_app(config=None):
     # Register Blueprints here
     app.register_blueprint(books_bp)
     app.register_blueprint(authors_bp)
+    app.register_blueprint(genres_bp)
 
     return app
--- app/models/book.py
+++ app/models/book.py
@@ -9,27 +9,34 @@ class Book(db.Model):
     description: Mapped[str]
     author_id: Mapped[Optional[int]] = mapped_column(ForeignKey("author.id"))
     author: Mapped[Optional["Author"]] = relationship(back_populates="books")
+    genres: Mapped[list["Genre"]] = relationship(secondary="book_genre", back_populates="books")
+    
+def to_dict(self):
+    book_as_dict = {}
+    book_as_dict["id"] = self.id
+    book_as_dict["title"] = self.title
+    book_as_dict["description"] = self.description
 
-    def to_dict(self):
-        book_as_dict = {}
-        book_as_dict["id"] = self.id
-        book_as_dict["title"] = self.title
-        book_as_dict["description"] = self.description
+    if self.author:
+        book_as_dict["author"] = self.author.name
 
-        if self.author:
-            book_as_dict["author"] = self.author.name
+    if self.genres:
+        book_as_dict["genres"] = [genre.name for genre in self.genres]
 
-        return book_as_dict
+    return book_as_dict
+
     
     @classmethod
-    def from_dict(cls, book_data):
-        # Use get() to fetch values that could be undefined to avoid raising an error
-        author_id = book_data.get("author_id")
+def from_dict(cls, book_data):
+    # Use get() to fetch values that could be undefined to avoid raising an error
+    author_id = book_data.get("author_id")
+    genres = book_data.get("genres", [])
 
-        new_book = cls(
-            title=book_data["title"],
-            description=book_data["description"],
-            author_id=author_id
-        )
+    new_book = cls(
+        title=book_data["title"],
+        description=book_data["description"],
+        author_id=author_id,
+        genres=genres
+    )
 
-        return new_book
+    return new_book
